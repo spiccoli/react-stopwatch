8416073af82da659b05074477392c3bd0856e1f2
admin <piccolisantiagonicolas@gmail.com>
2024-08-13 21:02:24 -0300

    intial commit

---
+++ src/Stopwatch.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState, useRef } from "react";
+
+function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  let intervalRef = useRef(null); // I make use of the object useRef to prevent potential misbehaviour later
+  let startTimeRef = useRef(0);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 11);
+    }
+    return () => {
+      clearInterval(intervalRef.current);
+    }; //cleanup
+  }, [isRunning]);
+
+  function start() {
+    startTimeRef.current = Date.now() - elapsedTime;
+    setIsRunning(true);
+  }
+  function pause() {
+    setIsRunning(false);
+  }
+  function reset() {
+    setElapsedTime(0);
+    setIsRunning(false);
+  }
+  function formatTime() {
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+      .toString()
+      .padStart(2, 0);
+    let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60)
+      .toString()
+      .padStart(2, 0);
+    let seconds = Math.floor((elapsedTime / 1000) % 60)
+      .toString()
+      .padStart(2, 0);
+    let miliseconds = Math.floor((elapsedTime % 1000) / 10)
+      .toString()
+      .padStart(2, 0);
+    return `${hours}:${minutes}:${seconds}:${miliseconds}`;
+  }
+  return (
+    <div className="stopwatch-container">
+      <div className="display">
+        <div className="display-time">{formatTime()}</div>
+        <button onClick={start}>start</button>
+        <button onClick={pause}>pause</button>
+        <button onClick={reset}>reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
